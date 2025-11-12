07694258d67e540420ccbb8b778d2c26282341f9
Yuske <vitor.yuski1@gmail.com>
2025-11-12 16:24:57 -0400

    Adicionando soluções do modulo 4

--- exercises/04_string_functions/07_solution.sql
+++ exercises/04_string_functions/07_solution.sql
@@ -6,6 +6,8 @@ DECLARE
 		RETURN substr(cpf, 1, 3);	  
 	end mask_cpf;
 BEGIN
-	dbms_output.put_line('Prefixo do CPF: ' || mask_cpf('123.456.789-00'));
-	
+
+	for b in (SELECT cpf from beneficiario) loop
+		dbms_output.put_line('Prefixo do CPF: ' || mask_cpf(b.cpf));
+	end loop;
 END;
